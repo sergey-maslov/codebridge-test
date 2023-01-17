f9c0aeb0f43c10a3cc5809f854ff2d47e7538a53
Sergey Maslov <shift.tfihs@gmail.com>
2023-01-17 12:32:46 +0200

    decomposition

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -1,64 +1,19 @@
 import React, { FC } from 'react';
 import { IArticle } from '../types/types';
 import './ArticlesList.scss';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Filter from './Filter';
-import { Link } from 'react-router-dom';
+import CardItem from './CardItem';
 
 interface ArticlesListProps {
     articles: IArticle[];
 }
 
-const theme = createTheme({
-    typography: {
-        fontFamily: 'Montserrat',
-    }
-});
-
 const ArticlesList: FC<ArticlesListProps> = ({ articles }) => {
-
 
     return (
         <div>
-            <div>
-                <Filter />
-            </div>
-            <div className="articles-list">
-                {articles.map(article =>
-                    <div key={article.id} className="card">
-                        <ThemeProvider theme={theme}>
-                            <Card>
-                                <CardMedia
-                                    component="img"
-                                    alt="green iguana"
-                                    height="217"
-                                    image={article.imageUrl}
-                                />
-                                <CardContent>
-                                    <Typography className="articles-list__title" gutterBottom variant="h5" component="div">
-                                        {article.title}
-                                    </Typography>
-                                    <Typography className="articles-list__summary" variant="body2" color="text.secondary">
-                                        {article.summary.substring(0, 99) + "..."}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Link to={'article/' + article.id}>
-                                        <Button size="small">Read more <ArrowForwardIcon /></Button>
-                                    </Link>
-                                </CardActions>
-                            </Card>
-                        </ThemeProvider>
-                    </div>
-                )}
-            </div>
+            <Filter />
+            <CardItem articles={articles} />
         </div>
     )
 }
---
+++ src/components/CardItem.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
+import { IArticle } from '../types/types';
+
+interface CardItemProps {
+    articles: IArticle[];
+}
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Montserrat',
+    }
+});
+
+const CardItem: FC<CardItemProps> = ({ articles }) => {
+    return (
+        <div className="articles-list">
+            {articles.map(article =>
+                <div key={article.id} className="card">
+                    <ThemeProvider theme={theme}>
+                            <Card>
+                                <CardMedia
+                                    component="img"
+                                    alt="green iguana"
+                                    height="217"
+                                    image={article.imageUrl}
+                                />
+                                <CardContent>
+                                    <Typography className="articles-list__title" gutterBottom variant="h6" component="div">
+                                        {article.title}
+                                    </Typography>
+                                    <Typography className="articles-list__summary" variant="body2" color="text.secondary">
+                                        {article.summary.substring(0, 99) + "..."}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Link to={'article/' + article.id}>
+                                        <Button size="small">Read more <ArrowForwardIcon /></Button>
+                                    </Link>
+                                </CardActions>
+                            </Card>
+                    </ThemeProvider>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default CardItem;
